52b59743990473d8d4c01e357ea67be87e3ef1ff
witonsz <witonskyzc@gmail.com>
2019-05-16 10:50:32 -0400

    Add name

--- src/m2_laptop_code.py
+++ src/m2_laptop_code.py
@@ -23,42 +23,41 @@ def get_my_frame(root, window, mqtt_sender):
     # DONE 2: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
-    # TODO: Put your GUI onto your frame (using sub-frames if you wish).
+    # DONE: Put your GUI onto your frame (using sub-frames if you wish).
 
     spin_left_label = ttk.Label(frame, text="Left wheel speed (0 to 100)")
     spin_right_label = ttk.Label(frame, text="Right wheel speed (0 to 100)")
-    number_spins_label = ttk.Label(frame, text="# spins")
+    dist_label = ttk.Label(frame, text="Degrees")
 
     spin_left_entry = ttk.Entry(frame, width=10, justify=tkinter.CENTER)
     spin_left_entry.insert(0, "100")
     spin_right_entry = ttk.Entry(frame, width=10, justify=tkinter.CENTER)
     spin_right_entry.insert(0, "100")
-    number_spins_entry = ttk.Entry(frame, width=15, justify=tkinter.CENTER)
-    number_spins_entry.insert(0, "3")
+    dist_entry = ttk.Entry(frame, width=15, justify=tkinter.CENTER)
+    dist_entry.insert(0, "3")
 
     left_button = ttk.Button(frame, text="Spin Left", width=10)
     right_button = ttk.Button(frame, text="Spin Right", width=10)
-    number_spins_button = ttk.Button(frame, text="Rotations", width=15)
+    dist_button = ttk.Button(frame, text="Rotations", width=15)
     stop_button = ttk.Button(frame, text="Stop", width=15)
 
     frame_label.grid(row=0, column=1)
     spin_left_label.grid(row=1, column=0)
     spin_right_label.grid(row=1, column=2)
-    number_spins_label.grid(row=1, column=1)
+    dist_label.grid(row=1, column=1)
 
     spin_left_entry.grid(row=2, column=0)
     spin_right_entry.grid(row=2, column=2)
-    number_spins_entry.grid(row=2, column=1)
+    dist_entry.grid(row=2, column=1)
 
     left_button.grid(row=3, column=0)
     right_button.grid(row=3, column=2)
-    number_spins_button.grid(row=3, column=1)
-    stop_button.grid(row=4, column=1)
+    stop_button.grid(row=3, column=1)
 
     left_button["command"] = lambda: handle_spin_left(
-        spin_left_entry, spin_right_entry, mqtt_sender)
+        spin_left_entry, spin_right_entry, dist_entry, mqtt_sender)
     right_button["command"] = lambda: handle_spin_right(
-        spin_left_entry, spin_right_entry, mqtt_sender)
+        spin_left_entry, spin_right_entry, dist_entry, mqtt_sender)
     # number_spins_button["command"] = lambda: handle_number_spins()
 
     stop_button["command"] = lambda: handle_stop(mqtt_sender)
@@ -83,39 +82,34 @@ class MyLaptopDelegate(object):
 
 # TODO: Add functions here as needed.
 
-def go(mqtt_sender, direction, spin_left_speed, spin_right_speed):
+def go(mqtt_sender, direction, spin_left_speed, spin_right_speed, dist_value):
     print()
     print(direction)
-    print("Left wheel motor speed:", spin_left_speed)
-    print("Right wheel motor speed:", spin_right_speed)
-    mqtt_sender.send_message("go", [spin_left_speed, spin_right_speed])
+    print("Left wheel motor speed:", spin_left_speed, "for", dist_value, "degrees")
+    print("Right wheel motor speed:", spin_right_speed, "for", dist_value, "degrees")
 
-def handle_spin_left(spin_left_box, spin_right_box, mqtt_sender):
+    mqtt_sender.send_message("go", [spin_left_speed, spin_right_speed, dist_value])
+
+def handle_spin_left(spin_left_box, spin_right_box, dist_box, mqtt_sender):
     left = -int(spin_left_box.get())
     right = int(spin_right_box.get())
-    go(mqtt_sender, "SPIN LEFT", left, right)
+    dist = int(dist_box.get())
+    go(mqtt_sender, "SPIN LEFT", left, right, dist)
 
-def handle_spin_right(spin_left_box, spin_right_box, mqtt_sender):
+def handle_spin_right(spin_left_box, spin_right_box, dist_box, mqtt_sender):
     left = int(spin_left_box.get())
     right = -int(spin_right_box.get())
-    go(mqtt_sender, "SPIN RIGHT", left, right)
+    dist = int(dist_box.get())
+    go(mqtt_sender, "SPIN RIGHT", left, right, dist)
 
 # def handle_number_spins():
 
 def handle_stop(mqtt_sender):
     print()
-    print("STOP.")
+    print("The movement has STOPPED.")
     mqtt_sender.send_message("stop", [])
 
-def send_forward(left, right, dist, mqtt):
-    i = 0
-    print('Sending Message: Forward', left, dist)
-    mqtt.send_message("forward", [left, right, dist])
 
-def send_backward(left, right, dist, mqtt):
-    i = 0
-    print('Sending Mesage: Backward', left, dist)
-    mqtt.send_message("backward", [left, right, dist])
 
-print()
+
 
